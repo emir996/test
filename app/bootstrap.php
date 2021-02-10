66589bec8b9a1055fdf9380dd657458a6f3efa09
emir996 <edzinovic96@gmail.com>
2021-02-10 20:39:52 +0100

    bootstraping all ap

---
+++ app/bootstrap.php
@@ -0,0 +1,11 @@
+<?php 
+// Load Config
+require_once 'config/config.php';
+// Load db config
+require_once 'config/database.php';
+
+use \app\libs\Core;
+
+// Init Core class
+$initCore = new Core;
+
