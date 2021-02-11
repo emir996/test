e749dbee692cac7035fd2787a3612ce5bf2f48d9
emir996 <edzinovic96@gmail.com>
2021-02-11 03:21:02 +0100

    bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,9 @@
 require_once 'config/config.php';
 // Load db config
 require_once 'config/database.php';
+// Load helpers
+require_once 'helpers/url_helper.php';
+require_once 'helpers/session_helper.php';
 
 use \app\libs\Core;
 
